c2df4747e33f1851bf09553a5c9754187706beea
Leonel Zalegas <EliasZalegas@gmail.com>
2023-03-17 18:18:45 -0300

    Agregamos clase UserService

---
+++ app/src/main/java/com/example/influencer/Data/Network/UserService.java
@@ -0,0 +1,21 @@
+package com.example.influencer.Data.Network;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class UserService {
+    public static final String USER_COLLECTION = "Usuarios";
+    private final FirebaseClient firebase;
+
+    public UserService(FirebaseClient firebase) {
+        this.firebase = firebase;
+    }
+
+    public Task<Void> crearUsuario(UsuarioSignin usuarioSignin){
+        return  firebase.getDb().collection(USER_COLLECTION).document(usuarioSignin.getId()).set(usuarioSignin);
+    }
+
+    public Task<DocumentSnapshot> getusuario(String id){
+        return firebase.getDb().collection(USER_COLLECTION).document(id).get();
+    }
+}
