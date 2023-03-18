0c0dfdbd63e008a75ec1d40fbdcc81a8fa35d927
Leonel Zalegas <EliasZalegas@gmail.com>
2023-03-18 15:50:52 -0300

    agregamos las interfaces Callback y GoogleSigninListener Tambien las clases GoogleSigninUseCase (ahi al constructor tambien agregamos lo que seria un parametro nuevo,AppCompatActivity, para asi poder usar registerForActivityResult que parece que solo funca con un Activity, especificamente esa subclase de ella)

---
+++ app/src/main/java/com/example/influencer/UI/GoogleSignin/Callback.java
@@ -0,0 +1,6 @@
+package com.example.influencer.UI.GoogleSignin;
+
+public interface Callback {
+    void onSuccess();
+    void onError();
+}
---
+++ app/src/main/java/com/example/influencer/UI/GoogleSignin/GoogleSigninListener.java
@@ -0,0 +1,6 @@
+package com.example.influencer.UI.GoogleSignin;
+
+public interface GoogleSigninListener {
+    void onUserAuthenticated();
+    void onError();
+}
