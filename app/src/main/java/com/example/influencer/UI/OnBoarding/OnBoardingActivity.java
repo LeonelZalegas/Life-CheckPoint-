f75a890d942ceef18591d730cb2bf3bd7c9abbc5
Leonel Zalegas <EliasZalegas@gmail.com>
2023-04-07 13:23:24 -0300

    -agregamos el codigo en OnBoardingActivity, OnboardingViewModel y en el java de los 2 fragments que hicimos
    todo funciona bien, abria que agregar el 3er fragment, hacer todo mas bonito, agregar lo de los dots y que al presionar el boton del start now nos dirija al home

--- app/src/main/java/com/example/influencer/UI/OnBoarding/OnBoardingActivity.java
+++ app/src/main/java/com/example/influencer/UI/OnBoarding/OnBoardingActivity.java
@@ -1,16 +1,52 @@
 package com.example.influencer.UI.OnBoarding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.influencer.R;
+import com.example.influencer.UI.Login.LoginActivity;
+import com.example.influencer.UI.OnBoarding.Fragments.OnboardingFragment1;
+import com.example.influencer.UI.OnBoarding.Fragments.OnboardingFragment2;
+import com.example.influencer.databinding.ActivityOnboardingBinding;
 
 public class OnBoardingActivity extends AppCompatActivity {
+
+    private ActivityOnboardingBinding binding;
+    private OnboardingViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding);
+        binding = ActivityOnboardingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        viewModel = new ViewModelProvider(this).get(OnboardingViewModel.class);
+
+        // Set up ViewPager2 adapter
+        OnboardingPagerAdapter pagerAdapter = new OnboardingPagerAdapter(this);
+        pagerAdapter.addFragment(new OnboardingFragment1());
+        pagerAdapter.addFragment(new OnboardingFragment2());
+        binding.onboardingViewPager.setAdapter(pagerAdapter);
+
+        binding.buttonStartNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewModel.StartnowSelected();
+            }
+        });
+
+        viewModel.navigateToHome.observe(this, event -> {
+            if (event != null && event.getContentIfNotHandled() != null) {
+                goTonavigateToHome();
+            }
+        });
+    }
+
+    private void goTonavigateToHome() {
+        //hace algo
     }
 }
--- app/src/main/java/com/example/influencer/UI/OnBoarding/OnboardingViewModel.java
+++ app/src/main/java/com/example/influencer/UI/OnBoarding/OnboardingViewModel.java
@@ -4,9 +4,15 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.influencer.Core.Event;
+
 public class OnboardingViewModel extends ViewModel {
 
+    private final MutableLiveData<Event<Boolean>> _navigateToHome = new MutableLiveData<>();
+    public LiveData<Event<Boolean>> navigateToHome  = _navigateToHome ;
 
-
+    public void StartnowSelected(){
+        _navigateToHome.postValue(new Event<>(true));
+    }
 }
 
