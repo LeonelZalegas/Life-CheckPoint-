a810444b21270a9b787c13d101bd4490193b81a7
Leonel Zalegas <EliasZalegas@gmail.com>
2023-04-29 15:18:27 -0300

    bueno, agregamos setupWithNavController y toda la demas wea para controlar los fragments segun lo que apretamos en el navigation bar

--- app/src/main/java/com/example/influencer/UI/Home.java
+++ app/src/main/java/com/example/influencer/UI/Home.java
@@ -1,16 +1,36 @@
 package com.example.influencer.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.widget.PopupMenu;
 
 import com.example.influencer.R;
+import com.example.influencer.databinding.ActivityHomeBinding;
+import com.example.influencer.databinding.ActivityOnboardingBinding;
+
+import me.ibrahimsn.lib.OnItemSelectedListener;
+import me.ibrahimsn.lib.SmoothBottomBar;
 
 public class Home extends AppCompatActivity {
+
+    private ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavController nav = Navigation.findNavController(this,  R.id.main_tab_container);
+        //nos pedian hacer esto en la docu de la liberia de Smoothnavbar, osea inflar el menu en vez del Bar (como sucede en bottom navigation view)
+        PopupMenu popupMenu = new PopupMenu(this, null);
+        popupMenu.inflate(R.menu.bottom_nav_bar_menu);
+
+        binding.bottomBar.setupWithNavController(popupMenu.getMenu(),nav);
+
     }
 }
