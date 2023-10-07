346ac0de80755fe6b347c166281aa2feacccc92d
Leonel Zalegas <EliasZalegas@gmail.com>
2023-10-07 14:55:59 -0300

    -creamos el UI para crear un post -las clases a partir de ahora (incluyendo las del new Post UI) van a ser en kotlin

---
+++ app/src/main/java/com/example/influencer/UI/Upload_New_Checkpoint/UploadNewCheckpoint.kt
@@ -0,0 +1,12 @@
+package com.example.influencer.UI.Upload_New_Checkpoint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.influencer.R
+
+class UploadNewCheckpoint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_upload_new_checkpoint)
+    }
+}
