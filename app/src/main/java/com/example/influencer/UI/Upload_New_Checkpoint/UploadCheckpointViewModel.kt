3f54c5343a8e17710145c705815efef08517eec9
Leonel Zalegas <EliasZalegas@gmail.com>
2024-02-06 17:36:26 -0300

    subir imagenes con camara y guardarlo temporalmente y luego en firebase: -UploadCheckpointViewModel -implementation para usar viewModelScope en Gradle

---
+++ app/src/main/java/com/example/influencer/UI/Upload_New_Checkpoint/UploadCheckpointViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.influencer.UI.Upload_New_Checkpoint
+
+import android.net.Uri
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.influencer.Domain.SavePostUseCase
+import com.example.influencer.Domain.UploadImageUseCase
+import com.example.influencer.UI.Upload_New_Checkpoint.Model.Post
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class UploadCheckpointViewModel @Inject constructor(
+    private val savePostUseCase: SavePostUseCase,
+    private val uploadImageUseCase: UploadImageUseCase
+) : ViewModel() {
+
+    var _tempImageUri1: Uri? = null
+    var _tempImageUri2: Uri? = null
+
+    fun onCameraIconClicked(photoUri: Uri?){
+        viewModelScope.launch{
+            try {
+                if (_tempImageUri1 == null) {
+                    _tempImageUri1 = photoUri
+                } else {
+                    _tempImageUri2 = photoUri
+                }
+            } catch (e: Exception){
+                println(e.message)
+            }
+        }
+    }
+
+    fun canTakeMorePictures(): Boolean {
+        return _tempImageUri1 == null || _tempImageUri2 == null
+    }
+
+    fun savePost(text: String, satisfactionLevel: Int){
+        viewModelScope.launch {
+            val image1Url = _tempImageUri1?.let { uploadImageUseCase(it)}
+            val image2Url = _tempImageUri2?.let { uploadImageUseCase(it)}
+
+            val post = Post(text, satisfactionLevel, image1Url, image2Url)
+            savePostUseCase(post)
+        }
+    }
+}
